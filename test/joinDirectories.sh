0ac767515c6879fa65ffe4b1f1001d9bd3976cb4
= <=>
2022-01-12 18:20:12 +0100

    working on a way to get directories of files. That done and the rest of the logic should be easy

--- test/joinDirectories.sh
+++ test/joinDirectories.sh
@@ -1,10 +1,32 @@
 #!/bin/sh
 
-dirOrigin=./repo;
+dirO=./repo;
+dir1=./shRepo/templates/common/None
+dir2=./shRepo/templates/Web/basic
 
 #Debug
-rm -rf $dirOrigin
-mkdir $dirOrigin
-git init $dirOrigin
+rm -rf $dirO
+mkdir $dirO
+cd $dirO
+git init > /dev/null
+cd - > /dev/null
+
+join() {
+	dirOrigin="$1"
+	dir="$2"
+	dirRegex="$(echo $2 | sed 's/\//\\\//g')"
+
+	echo "Joining the content of $dir into $dirOrigin"
+	
+	for f in $(find $dir -type f); do
+		f=$(printf $f | sed -E "s/$dir\///")
+		indir=$(printf $f | sed -E 's/.*([^\/]*)$/\1/')
+		echo "- Dir: $indir"
+		echo "  - $f"
+	done
+	echo "Done"
+}
 
 
+join "$dirO" "$dir1"
+join "$dirO" "$dir2"
