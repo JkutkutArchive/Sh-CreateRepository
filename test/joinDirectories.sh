826cf6ed7e5f948b06c5e7748a2462306f5032c7
Jkutkut <jorgeregonzalez@gmail.com>
2022-01-13 16:38:47 +0100

    applyTemplate funtion ready to join the rest of the logic

--- test/joinDirectories.sh
+++ test/joinDirectories.sh
@@ -2,7 +2,8 @@
 
 dirO=./repo;
 dir1=./shRepo/templates/common/None
-dir2=./shRepo/templates/Web/basic
+dir2=./shRepo/templates/C/42Madrid-Project
+#dir2=./shRepo/templates/Web/basic
 
 #Debug
 rm -rf $dirO
@@ -11,7 +12,8 @@ cd $dirO
 git init > /dev/null
 cd - > /dev/null
 
-join() {
+setupFileName="setup-project.sh"
+applyTemplate() {
 	dirOrigin="$1"
 	dir="$2"
 	dirRegex="$(echo $2 | sed 's/\//\\\//g')"
@@ -19,9 +21,7 @@ join() {
 	echo "Joining the content of $dir into $dirOrigin"
 	
 	for f in $(find $dir -type f); do
-		#echo "File raw: --$f--"
 		fileLocal=$(printf "$f" | sed "s/$dirRegex\///")
-		#echo "File: --$f--"
 		indir=$(printf $fileLocal | sed -E 's/(.*\/)?[^\/]*$/\1/')
 		
 		echo "- Dir: $indir"
@@ -30,9 +30,17 @@ join() {
 		mkdir -p $dirOrigin/$indir
 		cat $f >> $dirOrigin/$fileLocal
 	done
+	if [ -f $dirOrigin/$setupFileName ]; then
+		currentDir=$(pwd)
+		cd $dirOrigin
+		chmod 744 $setupFileName
+		./$setupFileName
+		rm ./$setupFileName
+		cd $currentDir
+	fi
 	echo "Done"
 }
 
 
-join "$dirO" "$dir1"
-join "$dirO" "$dir2"
+applyTemplate "$dirO" "$dir1"
+applyTemplate "$dirO" "$dir2"
